b5636ee1655eaf6b4d80c00282b1793d30d1ec01
Tobias Anker <tobias.anker@kitsunemimi.moe>
2023-02-07 23:41:03 +0100

    Feat/Fix (#31): Re-add ci-pipline within the new data-structure of the repo

--- build.sh
+++ build.sh
@@ -4,11 +4,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PARENT_DIR="$(dirname "$DIR")"
 
-# build Hanami-AI library with qmake
-/usr/lib/x86_64-linux-gnu/qt5/bin/qmake "$PARENT_DIR/Hanami-AI/Hanami-AI.pro" -spec linux-g++ "CONFIG += optimize_full staticlib"
+if [ $1 = "test" ]; then
+    /usr/lib/x86_64-linux-gnu/qt5/bin/qmake "$PARENT_DIR/Hanami-AI/Hanami-AI.pro" -spec linux-g++ "CONFIG += optimize_full staticlib run_tests"
+else
+    /usr/lib/x86_64-linux-gnu/qt5/bin/qmake "$PARENT_DIR/Hanami-AI/Hanami-AI.pro" -spec linux-g++ "CONFIG += optimize_full staticlib"
+fi
 
-# IMPORTNANT: at the moment it has to be build with only 1 thread, because the parser-generation with bison and flex
-#             has problems in a parallel build-process (see issue #30)
 /usr/bin/make -j8
 
 #-----------------------------------------------------------------------------------------------------------------
